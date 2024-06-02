ac553f98c4ee0f17f04d3d70031113ce8e7b32b2
Risdorn <rishabhindoria2203@gmail.com>
2024-06-02 21:39:08 +0530

    Made home page, and checked that app works with it.

--- app.py
+++ app.py
@@ -23,3 +23,11 @@ with app.app_context():
     db.create_all()
 
 # Home page
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
